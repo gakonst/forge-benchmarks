c3e88843e3dc917f191143c3ee115d6bb0de95a8
Georgios Konstantopoulos <me@gakonst.com>
2021-10-15 15:19:09 +0300

    run dapp update manually

--- test.sh
+++ test.sh
@@ -7,14 +7,22 @@ TESTDATA=testdata
 
 DIR=`pwd`
 
+# override if needed
+# function dapp() {
+#     cargo r --manifest-path ../../../dapptools-rs/Cargo.toml --bin dapp $@
+# }
+
 function runTests() {
     cd $TESTDATA/$1
-    # assumes the existence of a remappings.txt
-    export DAPP_REMAPPINGS=$(cat remappings.txt)
 
+    # run any installation step if needed
+    make install || true
+
+    dapp update
     # always have the ffi flag turned on
     # dapp test --ffi
     dapp test --ffi
+
     cd -
 }
 
